d89d2040f753c86e19cc498ab1a8f132911dd210
lawiparadise <lawiparadise@gmail.com>
2025-03-18 23:58:33 +0900

    feat: 씬 추가, Player추가, Input System 추가, visual studio package 설치

---
+++ NovaProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
