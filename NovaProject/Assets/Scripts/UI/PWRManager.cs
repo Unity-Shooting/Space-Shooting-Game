071eed21960950e4797c4e58f4181d5ef43b7567
DaePPang <disa13000@naver.com>
2025-03-26 06:17:24 +0900

    게임 UI, Pause 버튼 추가 및 기능 구현

--- NovaProject/Assets/Scripts/UI/PWRManager.cs
+++ NovaProject/Assets/Scripts/UI/PWRManager.cs
@@ -2,21 +2,52 @@ using System;
 using System.Collections;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PWRManager : MonoBehaviour
 {
 
+    // 싱글턴 인스턴스
+    public static PWRManager Instance { get; private set; }
+
     public Image Pwr;
     public Image Ammo;
     public Image[] images;
+    public Image Pause;
+    public GameObject PauseMenu;
+
+    public Button Resume;
+    public Button MainMenu;
+
+    public bool Run  { get; set; }
 
     public float duration;
 
+    private void Awake()
+    {
+        Run = true;
+        // 만약 이미 인스턴스가 존재하면, 현재 오브젝트를 파괴
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
 
+        // 인스턴스를 자신으로 설정
+        Instance = this;
+    }
 
     void Start()
     {
+        if (Run)
+        {
+            Debug.Log("Run : "+Run);
+            Pause.GetComponent<Button>().onClick.AddListener(PauseAction);
+        }
+        Resume.onClick.AddListener(OnResume);
+        MainMenu.onClick.AddListener(OnMainMenu);
+
         duration = PlayerController.Instance.shieldCoolTime;
 
         //오류 로그로 인한 수정
@@ -31,6 +62,60 @@ public class PWRManager : MonoBehaviour
         // ScoreManager.instance.AddScore(100); ScoreManager 인스턴스 테스트
     }
 
+    public void PauseAction()
+    {
+        if (Run && Time.timeScale == 1)  // 게임이 진행 중일 때
+        {
+            Debug.Log("PauseMenu 클릭 : ");
+            Time.timeScale = 0; // 게임을 일시 정지
+            PauseMenu.SetActive(true); // Pause 메뉴 표시
+        }
+        else if (Run && Time.timeScale == 0) // 게임이 일시 정지 중일 때
+        {
+            OnResume(); // 일시 정지 상태에서 Resume
+        }
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+
+    }
+    public void OnMainMenu()
+    {
+        Time.timeScale = 1;
+        LoadMainMenu();
+    }
+
+    /// <summary>
+    ///  메인 메뉴로 이동하는 함수
+    /// </summary>
+    /// 
+    private void LoadMainMenu()
+    {
+        Time.timeScale = 1; //  씬 변경 전 TimeScale을 다시 1로 설정
+        DestroyAllPersistentObjects();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /// <summary>
+    /// MainMenu Secen 으로 넘어갈때, DontDestroyOnLoad object 제거
+    /// </summary>
+    private void DestroyAllPersistentObjects()
+    {
+        GameObject[] allObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);     //씬에 있는 모든 오브젝트를 검색
+
+        foreach (GameObject obj in allObjects)  //모든 오브젝트를 foreach를 사용하여 순회하며 처리
+        {
+            //obj.scene.handle == -1로 해당 오브젝트가 DontDestroyOnLoad 씬에 속하는지 확인
+            if (obj.scene.buildIndex == -1) // DontDestroyOnLoad에 있는 오브젝트인지 확인
+            {
+                //해당 오브젝트를 삭제
+                Destroy(obj);
+            }
+        }
+    }
 
     void Update()
     {
@@ -116,6 +201,16 @@ public class PWRManager : MonoBehaviour
             yield return null; // 다음 프레임까지 대기
         }
         Ammo.fillAmount = 1f; // 10초가 되면 fillAmount를 정확히 1로 설정
+    }
+
+    public void StartPower()
+    {
+        Run = true;
+    }
+
+    public void StopPower()
+    {
+        Run = false;
     }
 
 }
