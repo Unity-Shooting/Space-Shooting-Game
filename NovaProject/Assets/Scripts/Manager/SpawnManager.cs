0cabb281e9fd948e964216769d5d726138e9254e
codo3356 <doni01131@gmail.com>
2025-03-29 18:26:38 +0900

    feat: SpawnManager의 StartStage 변경
    씬1에서 생성된걸 파괴하지 않고 계속 쓰기위해서
    모든 스태이지 정보를 처음부터 가져가고
    스태이지 변경시 StartStage(index) 호출

--- NovaProject/Assets/Scripts/Manager/SpawnManager.cs
+++ NovaProject/Assets/Scripts/Manager/SpawnManager.cs
@@ -60,7 +60,7 @@ public class WaveData
 public class SpawnManager : Singleton<SpawnManager>
 {
     [SerializeField] private GameObject boss;
-    [SerializeField] private StageWaveSO stage;  // 하나의 스테이지동안 실행될 여러 WavaData의 리스트를 가지고있음
+    [SerializeField] private StageWaveSO[] stage;  // 하나의 스테이지동안 실행될 여러 WavaData의 리스트를 가지고있음
 
     [Header("아이템 프리펩")]
 
@@ -70,15 +70,19 @@ public class SpawnManager : Singleton<SpawnManager>
     [SerializeField] protected GameObject itemPower;
     void Start()
     {
-        StartStage();   // 테스트용 코드. 스테이지 스폰 시작
     }
     /// <summary>
     /// 해당 스테이지의 스폰을 시작
     /// </summary>
     /// <param name="i"></param>
-    void StartStage()
+    void StartStage(int index)
     {
-        StartCoroutine(WaveStarter(stage.waves));
+        if(index <= 0|| index >= stage.Length)
+        {
+            Debug.LogError("해당 스테이지가 없습니다");
+            return;
+        }
+        StartCoroutine(WaveStarter(stage[index].waves));
     }
 
     /// <summary>
